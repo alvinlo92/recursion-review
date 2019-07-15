e5edbfd2aae1b823276864b93d41a107d6af942b
alvinlo92 <alvinlo92@yahoo.com>
2019-07-15 12:33:27 -0700

    Began psudocoding

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,3 +6,13 @@
 var stringifyJSON = function(obj) {
   // your code goes here
 };
+
+
+// if string, number, or boolean
+  // string concatnation
+// if undefined or funtion
+  // return undefined
+// if array
+  // for loop and run recursion on each element
+// if object
+  // for in loop and run recursion on each key value
